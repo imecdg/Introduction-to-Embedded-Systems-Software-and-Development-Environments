4ba8a55dd075ba68c8f3ee52c4c22ab952d0d57d
Erick Davalos <erick_ram@live.com.mx>
2023-12-04 15:40:37 -0700

    FInished assesment header file

--- m1/stats.h
+++ m1/stats.h
@@ -24,17 +24,47 @@
 /* Add Your Declarations and Function Comments here */ 
 
 /**
- * @brief <Add Brief Description of Function Here>
- *
- * <Add Extended Description Here>
- *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
+ * @brief Prints the statistics of a given array
  */
 
+void print_array(unsigned char *array,unsigned int asize);
+
+/*
+ * @brief prints original array
+ * @param array is the content of the array and asize is the lenght
+ * @return nothing just printing
+ * */
+void print_statistics(unsigned char minimum, unsigned char maximum, float mean, unsigned char median);
+/*
+ * @brief it does print all the calculated values 
+ * @param minium is the minimum value of the array, maximum the max value of the array, the mean and median values
+ * @ it dont returns anything just printing
+ * */
+unsigned char find_median(unsigned char *array, unsigned int asize);
+/*
+ *@brief it calculates the median value of the array
+ *@param *array and asize, values and length of the array respectively
+ * */
+float find_mean(unsigned char *array, unsigned int asize);
+/*
+ * @brief it calculates the mean of the array
+ *@param *array and asize, values and length of the array respectively
+ * */
+unsigned char find_minimum(unsigned char *array, unsigned int asize);
+
+/*
+ * @brief it finds the minimum value in the array
+ *@param *array and asize, values and length of the array respectively
+ * */
+unsigned char find_maximum(unsigned char *array, unsigned int asize);
+/*
+ * @brief it finds the max value in the array
+ *@param *array and asize, values and length of the array respectively
+ * */
+void sort_array(unsigned char *array, unsigned int asize);
+/*
+ * it sorts the array from max to min
+ *@param *array and asize, values and length of the array respectively
+ * */
 
 #endif /* __STATS_H__ */
