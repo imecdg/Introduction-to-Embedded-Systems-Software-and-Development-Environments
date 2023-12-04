1b70afe4b099229429fca858099fb0babebf3e3c
Erick Davalos <erick_ram@live.com.mx>
2023-12-04 15:39:00 -0700

    Just finishing assesments, it took more time than expected but I did it.

--- m1/stats.c
+++ m1/stats.c
@@ -35,9 +35,158 @@ void main() {
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+ /* Other Variable Declarations Go Here */
 
+ unsigned char minimum = 0;
+ unsigned char maximum = 0;
+ float mean = 0 ;
+ unsigned char median = 0;
+
+ /* Statistics and Printing Functions Go Here */
+
+ printf("Array before sorting\n");
+ print_array(test,SIZE);
+ sort_array(test, SIZE);
+ printf("Array after sorting: \n");
+ print_array(test, SIZE);
+ median = find_median(test,SIZE);
+ mean = find_mean(test, SIZE);
+ maximum = find_maximum(test, SIZE);
+ minimum = find_minimum(test, SIZE);
+ print_statistics(minimum, maximum, mean, median);
+	 
 }
 
 /* Add other Implementation File Code Here */
+/* Add other Implementation File Code Here */
+
+void print_array(unsigned char *array,unsigned int asize)
+{
+	for(int i=0;i<asize;i++)
+	{
+		printf("%d ,",*(array+i));
+	}
+	printf ("\n");
+	
+}
+
+
+void print_statistics(unsigned char minimum, unsigned char maximum, float mean, unsigned char median)
+{
+ 
+	printf("The minimum value in the array is %d \n", minimum);
+ 	printf("The maximum value in the array is %d \n ", maximum);
+ 	printf("The mean value in the array is %f \n", mean);
+ 	printf("The median value in the array is %d \n", median);
+
+}
+
+unsigned char find_median(unsigned char *array, unsigned int asize)
+{
+	unsigned char median = 0;
+	if(asize % 2 == 0)
+	{
+		median = (array[asize/2-1]+array[asize/2])/2;
+	}
+	else
+	{
+		median = array[asize/2];
+	}
+	return median;
+}
+float find_mean(unsigned char *array, unsigned int asize)
+
+{
+	float mean = 0.0f;
+	for(unsigned int i=0;i<asize;i++)
+	{
+		mean+=*(array+i);
+	}
+	mean = mean/asize;
+	return mean;
+}
+unsigned char find_minimum(unsigned char *array, unsigned int asize)
+{
+	unsigned char minimum = array[0];
+	for (int i=1;i<asize;i++)
+	{
+		if(*(array+i)<minimum)
+		{
+			minimum = *(array+i);
+		}
+		else
+		{
+		/* DO NOTHING
+		*/
+		}
+	}
+	return minimum;
+}
+unsigned char find_maximum(unsigned char *array, unsigned int asize)
+{
+	unsigned char maximum = array[0]; /* Initialize the variable with the first value of the array */
+	for(int i=1;i<asize;i++) /* Startin for from the second value in the array */
+	{
+		if(*(array+i)>maximum) /* Comparing 1 and 2 value of the array */
+		{
+			maximum = *(array+i);
+		}
+	
+unsigned char find_minimum(unsigned char *array, unsigned int asize)
+{
+        unsigned char minimum = array[0];
+        for (int i=1;i<asize;i++)
+        {
+	                if(*(array+i)<minimum)
+	                {
+			                        minimum = *(array+i);
+			                }
+	                else
+	                {
+			                /* DO NOTHING
+					 *                 */
+			                }
+	        }
+        return minimum;
+}
+unsigned char find_maximum(unsigned char *array, unsigned int asize)
+{
+        unsigned char maximum = array[0]; /* Initialize the variable with the first value of the array */
+        for(int i=1;i<asize;i++) /* Startin for from the second value in the array */
+        {
+	                if(*(array+i)>maximum) /* Comparing 1 and 2 value of the array */
+	                {
+			                        maximum = *(array+i);
+			                }
+	                else
+	                {
+			                        /* DO NOTHING */
+			                }
+	        }
+        return maximum;
+}
+}
+	return maximum;
+}
+void sort_array(unsigned char *array, unsigned int asize)
+{
+	unsigned int flag = 0;
+	unsigned char temp = 0;
+	do
+	{
+		flag = 0; /* Set flag to 0 every new operation */
+		for(int i = 0; i < asize-1; i++)
+		{
+			 if (array[i] < array[i +1])
+			{
+				/*swap elements*/
+				temp = array[i];
+				array[i] = array[i+1];
+				array[i+1]=temp;
+				flag = 1;
+			}
+		}
+	}
+	while (flag == 1);
+}
+	
